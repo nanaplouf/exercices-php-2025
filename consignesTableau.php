89406e2665a9f7f2e757fc00f791f7ab2f9fbcdb
nanaplouf <vera78ds@gmail.com>
2025-09-03 15:50:16 +0200

    correction 4

---
+++ consignesTableau.php
@@ -0,0 +1,42 @@
+<?php
+ //Créer un fichier ex-05-tableau.php
+
+
+/**Exercice 1 : Créer un tableau simple
+ *  Objectif : Créez un tableau contenant les noms de vos cinq films préférés.
+ *  Instructions :
+ *  Déclarez le tableau avec les noms des films.
+ *  Affichez dans un var_dump votre tableau
+ */
+
+/** Exercice 2 : Créer et afficher un tableau associatif
+ *   Objectif : Travaillez avec un tableau associatif.
+ *   Instructions :
+ *   Créez un tableau associatif qui contient les informations suivantes : prénom, nom, et âge.
+ *   Affichez chaque information avec une phrase descriptive.
+ */
+
+
+/**Exercice 3 : Compter les éléments d'un tableau
+ *  Objectif : Utilisez les fonctions count() et sizeof().
+ *  Instructions :
+ *  Créez un tableau avec plusieurs villes.
+ *  Affichez le nombre d'éléments dans le tableau.
+ */
+
+
+/** Exercice 4 : Boucle for pour parcourir un tableau
+ *   Objectif : Utilisez une boucle for pour parcourir un tableau.
+ *   Instructions :
+ *   Créez un tableau avec les mois de l'année.
+ *   Utilisez une boucle for pour afficher chaque mois.
+ */
+
+/** Exercice 5 : Boucle foreach pour parcourir un tableau associatif
+ *   Objectif : Utilisez une boucle foreach pour parcourir un tableau associatif.
+ *   Instructions :
+ *   Créez un tableau associatif avec des noms d'animaux et leurs bruits (par exemple, "chien" => "aboiement").
+ *   Utilisez une boucle foreach pour afficher chaque animal et son bruit.
+ */
+
+
---
+++ ex-04-condition.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Condition</title>
+</head>
+<body>
+    <h1>Exercices Conditions</h1>
+    <h2>Exercice 1</h2>
+    <?php
+        $age = 156;
+        if(($age >= 0) && ($age <= 120) && (is_int($age))){
+            echo "<p>L'age est valide</p>";
+        }else{
+            echo "<p>L'age n'est pas valide</p>";
+        }
+        var_dump(is_int($age));
+    ?>
+    <h2>Exercice 2 </h2>
+    <?php
+        $sunhour = 1; 
+     
+        switch ($sunhour) {
+            case 1 :
+                echo '<p>We are now monday</p>';
+            break;
+
+            case 2 :
+                echo '<p>we are now tuesday</p>';
+            break;
+
+            case 3 :
+                echo '<p>we are now wednesday</p>';
+            break;
+
+            case 4 :
+                echo '<p>we are now thursday</p>';
+            break;
+
+            case 5 :
+                echo '<p>we are now friday</p>';
+            break;
+
+            case 6 :
+                echo '<p>we are now saturday</p>';
+            break;
+
+            default:
+                echo '<p>we are now sunday</p>';
+            break;
+        }
+    ?>
+    
+    <h2>Exercice 3</h2>
+    <?php
+        $t1 = 1;
+        $t2 = "1";
+
+        if($t1 === $t2){
+            echo "<p> Vrai </p>";
+        } else {
+            echo "<p> Faux </p>";
+        };
+    ?>
+    <h2>Exercice 4</h2>
+    <?php
+        $var= "ALICE";
+        if (isset($var)){
+            echo "<p>La variable est definie et sa valeur est de : $var  </p>";
+        }else{
+            echo "<p>La variable n'est pas definie ou elle est null</p>";
+        }
+    ?>
+    <h2>Exercice 5</h2>
+    <?php
+        $nom = "";
+        if(empty($nom)){
+            echo "<p>Veuillez remplir le champ</p>";
+        }else{
+            echo "<p>Bienvenue $nom !</p>";
+        }
+    ?>
+
+    <h2>Exercice 6</h2>
+    <?php
+        $digital = true;
+        $mdp = true;
+
+        if((!empty($digital)) && (!empty($mdp))){
+           if ($digital XOR $mdp){
+                echo "<p>Tu peux te connecter</p>";
+            }else{
+                echo "<p>Tu ne peux pas te connecter</p>";
+            } 
+        }
+    ?>
+</body>
+</html>
+        
