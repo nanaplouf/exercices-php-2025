56ba7a14e63ea74019cabfcfb434ae2715e4cfd6
nanaplouf <vera78ds@gmail.com>
2025-09-03 12:16:41 +0200

    exo 4 correction 3

---
+++ 04-consignesCondition.php
@@ -0,0 +1,38 @@
+<?php
+ //Créer un fichier ex-04-condition.php
+
+/** Exercice 1 : Vérifier la validité d'un âge
+ *
+ *  Ecrire une condition qui vérifie si une variable age est valide (entre 0 et 120)
+ *
+ *  Afficher un message indiquant si l'âge est valide ou non 
+ * 
+ * petit bonus : vérifier que l'âge est un nombre entier et non un décimal
+ */
+
+/** Exercice 2 : Afficher le jour de la semaine
+ *  Ecrire une condition 'switch' pour afficher un message en fonction du jour de la semaine, le jour est donnée par une variable $jour en number (1 pour lundi, 2 pour mardi etc...)
+ */
+
+/** Exercice 3 : Comparaison de chaines de caractères
+ *  Ecrire une condition qui compare si deux variables sont identiques, la condition doit vérifier le type ET la valeur
+ */
+
+/** Exercice 4 : Tester une variable indéfinie
+ *  Ecrire une condition qui utilisera 'isset()' pour vérifier si un variable $var est définie, si elle est définie, afficher sa valeur, sinon afficher un message indiquant qu'elle n'est pas définie
+ * 
+ * Tentez avec null, '', 0 
+ */
+
+/** Exercice 5 : Valider un formulaire
+ *  Ecrire une condition qui vérifie si une variable $nom est vide avec empty(), si c'est le cas, afficher un message qui demandera à l'utilisateur de remplir le champ
+ */
+
+/** Exercice 6 : Vérifier la cohérence des réponses avec l'opérateur XOR
+ * Ecrire des conditions et vérifier la cohérence de ces réponses
+ *
+ *  Exemple : Nous avons une vérification a faire pour vérifier si l'utilisateur se connecte avec son empreinte digitale ou son mdp (il ne peut pas faire les deux en même temps)
+ *
+ *  xor : L'une des deux conditions doit être vraie seulement, si les deux sont vraies, alors il retournera false
+ *  contrairement à || (or) qui vérifiera si au moins l'une des deux conditions est vraie, même si les deux le sont
+ */
---
+++ ex-03-Arithmetique.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arithmetique</title>
+</head>
+<body>
+    <h1>Exercices d'arithmetique</h1>
+    <h2>Exercice 1</h2>
+    <?php
+        $a = 10;
+        $b = 2;
+        $total = $a + $b;
+        echo "<p>a+b=$total</p>";
+
+        $total = $a - $b;
+        echo "<p>a-b=$total</p>";
+
+        $total = $a * $b;
+        echo "<p>a*b=$total</p>";
+
+        $total = $a / $b;
+        echo "<p>a/b=$total</p>";
+
+        $total = $a % $b;
+        echo "<p>a%b=$total</p>";
+    ?>
+    <h2>Exercice 2</h2>
+    <?php
+        $c = 13;
+        $d = 7;
+        $c += $d;
+        echo "<p>c + d = $c</p>" ;
+        $c -= $d;
+        echo "<p>c - d = $c</p>" ;
+        $c *= $d;
+        echo "<p>c * d = $c</p>" ;
+        $c /= $d;
+        echo "<p>c / d = $c</p>" ;
+        $c %= $d;
+        echo "<p>c % d = $c</p>" ;
+    ?>
+
+    <h2>Exercice 3</h2>
+
+    <?php
+        $counter = 10;
+        $counter++;
+
+        $counter = 10;
+        $counter--;
+        echo "<p> Le compteur est à : $counter.</p>";
+    ?>
+
+    <h2>Exercice 4</h2>
+
+    <?php
+        $num = 10;
+        --$num; 
+        $num = 10;
+        ++$num;
+        echo "<p>Le résultat $num</p>";
+    ?>
+    
+    <h2>Exercice 5</h2>
+
+    <?php
+       $age = 40;
+       $age++;
+
+       $age -= 1;
+       echo "<p>$age</p>";
+    ?>
+
+    <h2>Exercice 6</h2>
+    <?php
+        $quantity = 5;
+        $quantity += 3;
+        $quantity -= 2;
+        echo "<p>Voici mon résultat : $quantity</p>";
+    ?>
+    
+    <h2>Exercice 7</h2>
+    <?php
+        $score = 50;
+        $score += 10;
+        echo "<p>Le total = $score </p>";
+        $score -= 5;
+        echo "<p>Le total = $score </p>";
+    ?>
+
+</body>
+</html>
