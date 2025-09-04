e9d155dfe01d9452a2cb1a6dedc0d383d4a10166
nanaplouf <vera78ds@gmail.com>
2025-09-04 11:55:39 +0200

    mise a jour exo 7 et 8

---
+++ 07-consigneFonction.php
@@ -0,0 +1,35 @@
+<?php
+ //Créer un fichier ex-07-fonction.php
+ 
+/*
+
+1. Fonction sans paramètres et sans valeur de retour
+Exercice : Créez une fonction appelée greet() qui affiche "Hello, world!" lorsqu'elle est appelée.
+*/
+
+
+/*
+2. Fonction avec paramètres et sans valeur de retour
+Exercice : Créez une fonction appelée greetUser() qui prend un paramètre $name et affiche "Hello, [name]!" où [name] est la valeur passée à la fonction.
+*/
+
+
+
+/*
+3. Fonction avec paramètres et avec valeur de retour
+Exercice : Créez une fonction appelée sum() qui prend deux paramètres $a et $b, les additionne, et retourne le résultat.
+*/
+
+/*
+4. Fonction avec paramètre par défaut
+Exercice : Créez une fonction appelée greetWithTime() qui prend deux paramètres, un nom $name et un moment de la journée $timeOfDay (par défaut "day"), et qui affiche "Good [timeOfDay], [name]!
+
+Rappel = vous pouvez ajouter une valeur au paramètre de fonction directement comme on a vu (exemple : function salut($name='Cynthia'){echo "Salut $name";})
+".
+*/
+
+/*
+5. Fonction avec une condition complexe
+Exercice : Créez une fonction appelée checkEligibility($age, $hasLicense) qui prend en paramètre un âge $age et un booléen $hasLicense. La fonction doit retourner "Eligible" si l'utilisateur a 18 ans ou plus et possède un permis de conduire, sinon "Not Eligible".
+
+*/
---
+++ 08-consignesDate.php
@@ -0,0 +1,33 @@
+<?php
+ //Créer un fichier ex-8-date.php
+
+
+/** Exercice 1 : Afficher la date actuelle
+ * 
+ *  Objectif : Afficher la date sous le format 'jour/mois/annee' suivi de 'heure:minute:secondes'
+ * 
+ *  aide : utiliser la fonction date()
+ */
+
+/** Exercice 2 : Formater une date
+ * 
+ *  Objectif : Transformer la date '2024-08-08' en format de type 08/08/2024
+ * 
+ *  aide : date() combinée à strtotime()
+ */
+
+/** Exercice 3 : Calculer l'âge d'une personne
+ * 
+ *  Objectif : Ecrire un script qui calcul l'age d'une personne née le '15-02-1990'
+ * 
+ *  aide : combinaison date() et strtotime()
+ * 
+ */
+
+/** Exercice 4 : Valider une date
+ * 
+ *  Objectif : Ecrire une fonction qui valide si une date donnée sous la forme 'dd-mm-yyyy' est valide ou non
+ * 
+ *  Aide : Utiliser strtotime() et verifier si le resultat est false
+ * 
+ */
