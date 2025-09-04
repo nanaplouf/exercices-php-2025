a77cc759c60eea185cd34758cffc4ae86dda69cd
nanaplouf <vera78ds@gmail.com>
2025-09-04 11:50:32 +0200

    exo boucle

---
+++ 06-consignesBoucle.php
@@ -0,0 +1,57 @@
+<?php
+ //Créer un fichier ex-06-boucle.php
+
+// Afficher toutes les réponses
+
+/** Exercice 1 : Boucle while basique
+ * 
+ *  Objectif : Créer une boucle while qui affiche les nombres pairs de 0 à 20
+ * 
+ */
+
+/** Exercice 2 : Générer une liste d'années avec une boucle while
+ * 
+ *  Ojectif : Afficher les années de 2000 à l'année actuelle (qui doit être incluse) dans une liste non ordonnée (<ul>)
+ * 
+ */
+
+/** Exercice 3 : Boucle do...while
+ * 
+ *  Objectif : Utiliser une boucle do...while pour afficher les multiples de 3 inférieurs à 30. S'assurer que le premier multiple est affiché même si la condition n'est pas remplie
+ * 
+ * 
+ */
+
+/** Exercice 4 : foreach pour un tableau associatif
+ * 
+ *  Objectif : Créer un tableau associatif avec les informations suivantes : 'prenom','nom','email','age'
+ *             Afficher chaque information sous la forme clé : valeur dans des paragraphes, l'email doit être dans un lien (<a>)
+ * 
+ */
+
+/** Exercice 5 : Boucles imbriquées et conditions
+ * 
+ *  Objectif : Créer un tableau HTML de 10x10 dans lequel chaque cellule contient un nombre aléatoire entre 1 et 100. 
+ * 
+ *  Mettre un background vert sur les cellules contenant un nombre pair
+ * 
+ */
+
+/** Exercice 6 : Tableau de tableaux
+ * 
+ *  Objectif : Créer un tableau contenant trois sous tableaux, chacun représentera une personne avec les clés 'prenom','nom','age'. 
+ * 
+ *  Afficher toutes les informations sous forme de liste HTML ordonnées ('<ol>'), où chaque personne a sa propre sous-liste (<ul>)
+ * 
+ *  Résultat attendu : 
+ * 
+ *  <ol> 
+ *  <li> Personne 1 </li>
+ *  <ul> 
+ *  <li> prénom : prenom </li>
+ *  <li> nom : nom </li>
+ *  <li> age: age </li>
+ *  </ul>
+ *  <li> Personne 2 </li>
+ * 
+ */
